bbdc6113e2e6cd769433cfba52e28ce3be88db6e
Amadeus-ITUi <aaron20051105@gmail.com>
2025-11-18 17:37:26 +0800

    马尔科夫决策过程 和 马尔科夫奖励过程 和 蒙特卡洛采样 和 占用度量计算

--- demo02/Markov.py
+++ demo02/Markov.py
@@ -1,23 +1,84 @@
 import numpy as np
 
 np.random.seed(0)
-P = [
-    [0.9, 0.1, 0.0, 0.0, 0.0, 0.0],
-    [0.5, 0.0, 0.5, 0.0, 0.0, 0.0],
-    [0.0, 0.0, 0.0, 0.6, 0.0, 0.4],
-    [0.0, 0.0, 0.0, 0.0, 0.3, 0.7],
-    [0.0, 0.2, 0.3, 0.5, 0.0, 0.0],
-    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-]
-P = np.array(P)
+# P = [
+#     [0.9, 0.1, 0.0, 0.0, 0.0, 0.0],
+#     [0.5, 0.0, 0.5, 0.0, 0.0, 0.0],
+#     [0.0, 0.0, 0.0, 0.6, 0.0, 0.4],
+#     [0.0, 0.0, 0.0, 0.0, 0.3, 0.7],
+#     [0.0, 0.2, 0.3, 0.5, 0.0, 0.0],
+#     [0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+# ]
+# P = np.array(P)
 
-rewards = [-1, -2, -3, 10, 1, 0]
+# rewards = [-1, -2, -3, 10, 1, 0]
+# gamma = 0.5
+
+S = ["s1", "s2", "s3", "s4", "s5"]
+A = ["保持s1", "前往s1", "前往s2", "前往s3", "前往s4", "前往s5", "概率前往"]
+P = {
+    "s1-保持s1-s1": 1.0,
+    "s1-前往s2-s2": 1.0,
+    "s2-前往s1-s1": 1.0,
+    "s2-前往s3-s3": 1.0,
+    "s3-前往s4-s4": 1.0,
+    "s3-前往s5-s5": 1.0,
+    "s4-前往s5-s5": 1.0,
+    "s4-概率前往-s2": 0.2,
+    "s4-概率前往-s3": 0.4,
+    "s4-概率前往-s4": 0.4,
+}
+R = {
+    "s1-保持s1": -1,
+    "s1-前往s2": 0,
+    "s2-前往s1": -1,
+    "s2-前往s3": -2,
+    "s3-前往s4": -2,
+    "s3-前往s5": 0,
+    "s4-前往s5": 10,
+    "s4-概率前往": 1,
+}
 gamma = 0.5
+MDP = (S, A, P, R, gamma)
+
+Pi_1 = {
+    "s1-保持s1": 0.5,
+    "s1-前往s2": 0.5,
+    "s2-前往s1": 0.5,
+    "s2-前往s3": 0.5,
+    "s3-前往s4": 0.5,
+    "s3-前往s5": 0.5,
+    "s4-前往s5": 0.5,
+    "s4-概率前往": 0.5,
+}
+Pi_2 = {
+    "s1-保持s1": 0.6,
+    "s1-前往s2": 0.4,
+    "s2-前往s1": 0.3,
+    "s2-前往s3": 0.7,
+    "s3-前往s4": 0.5,
+    "s3-前往s5": 0.5,
+    "s4-前往s5": 0.1,
+    "s4-概率前往": 0.9,
+}
+
+P_from_mdp_to_mrp = [
+    [0.5, 0.5, 0.0, 0.0, 0.0],
+    [0.5, 0.0, 0.5, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.5, 0.5],
+    [0.0, 0.1, 0.2, 0.2, 0.5],
+    [0.0, 0.0, 0.0, 0.0, 1.0],
+]
+P_from_mdp_to_mrp = np.array(P_from_mdp_to_mrp)
+R_from_mdp_to_mrp = [-0.5, -1.5, -1.0, 5.5, 0]
+
+def join(str1, str2):
+    return str1 + '-' + str2
 
 def compute_return(start_index, chain, gamma):
     G = 0
     for i in reversed(range(start_index, len(chain))):
-        G = gamma * G + rewards[chain[i] - 1]
+        G = gamma * G + R[chain[i] - 1]
     return G
 
 def compute(P, rewards, gamma, states_num):
@@ -25,10 +86,88 @@ def compute(P, rewards, gamma, states_num):
     value = np.dot(np.linalg.inv(np.eye(states_num, states_num) - gamma * P), rewards)
     return value
 
+def sample(MDP, Pi, timestep_max, number):
+    S, A, P, R, gamma = MDP
+    episodes = []
+    for _ in range(number):
+        episode = []
+        timestep = 0
+        s = S[np.random.randint(4)]
+        while s != "s5" and timestep <= timestep_max:
+            timestep += 1
+            rand, temp = np.random.rand(), 0
+            for a_opt in A:
+                temp += Pi.get(join(s, a_opt), 0)
+                if temp >= rand:
+                    a = a_opt
+                    r = R.get(join(s, a), 0)
+                    break
+            rand, temp = np.random.rand(), 0
+            for s_opt in S:
+                temp += P.get(join(join(s, a),s_opt), 0)
+                if temp > rand:
+                    s_next = s_opt
+                    break
+            episode.append((s, a, r, s_next))
+            s = s_next
+        episodes.append(episode)
+    return episodes
+
+def MC(episodes, V, N, gamma):
+    for episode in episodes:
+        G = 0
+        for i in range(len(episode) - 1, -1, -1):
+            (s, a, r, s_next) = episode[i]
+            G = gamma * G + r 
+            N[s] = N[s] + 1
+            V[s] = V[s] + (G - V[s]) / N[s]                   
+
+def occupancy(episodes, s, a, timestep_max, gamma):
+    rho = 0
+    total_times = np.zeros(timestep_max)
+    occur_times = np.zeros(timestep_max)
+    for episode in episodes:
+        for i in range(len(episode)):
+            (s_opt, a_opt, r, s_next) = episode[i]
+            total_times[i] += 1
+            if s == s_opt and a == a_opt:
+                occur_times[i] += 1
+    for i in reversed(range(timestep_max)):
+        if total_times[i]:
+            rho += gamma**i * occur_times[i] / total_times[i]
+    return (1 - gamma) * rho
+
 # chain = [1, 2, 3, 6]
 # start_index = 0
 # G = compute_return(start_index, chain, gamma)
 # print("Return G:", G)   
 
-V = compute(P, rewards, gamma, 6)
-print("Value function V:\n", V)
+# V = compute(P, rewards, gamma, 6)
+# print("Value function V:\n", V)
+
+# V = compute(P_from_mdp_to_mrp, R_from_mdp_to_mrp, gamma, 5)
+# print("Value function V from MDP to MRP:\n", V)
+
+# episodes = sample(MDP, Pi_2, 20, 5)
+# print('第一条序列\n', episodes[0])
+# print('第二条序列\n', episodes[1])
+# print('第三条序列\n', episodes[2])
+# print('第四条序列\n', episodes[3])
+# print('第五条序列\n', episodes[4])
+
+# timestep_max = 20
+# episodes = sample(MDP, Pi_1, timestep_max, 10000)
+# gamma = 0.5
+# V = {"s1": 0, "s2": 0, "s3": 0, "s4": 0, "s5": 0}
+# N = {"s1": 0, "s2": 0, "s3": 0, "s4": 0, "s5": 0}
+# MC(episodes, V, N, gamma)
+# print("Value function V from MC:\n", V)
+
+gamma = 0.5
+timestep_max = 1000
+episodes_1 = sample(MDP, Pi_1, timestep_max, 10000)
+episodes_2 = sample(MDP, Pi_2, timestep_max, 10000)
+rho_1 = occupancy(episodes_1, "s4", "概率前往", timestep_max, gamma)
+rho_2 = occupancy(episodes_2, "s4", "概率前往", timestep_max, gamma)
+print("Occupancy measure rho under Pi_1:", rho_1)
+print("Occupancy measure rho under Pi_2:", rho_2)
